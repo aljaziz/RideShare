c76a75ea1521609c99e066ca7eb80405a3348745
aljaziz <turjatheharbingerofdeath@gmail.com>
2026-06-22 18:38:58 +0600

    graceful shutdown added to trip service server

---
+++ services/trip-service/cmd/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	h "aljaziz/RideShare/services/trip-service/internal/infrastructure/http"
+	"aljaziz/RideShare/services/trip-service/internal/infrastructure/repository"
+	"aljaziz/RideShare/services/trip-service/internal/service"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	inmemRepo := repository.NewInmemRepository()
+	svc := service.NewService(inmemRepo)
+	mux := http.NewServeMux()
+
+	httphandler := h.HttpHandler{Service: svc}
+
+	mux.HandleFunc("POST /preview", httphandler.HandleTripPreview)
+
+	server := &http.Server{
+		Addr:    ":8083",
+		Handler: mux,
+	}
+
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Printf("Server listening on %s", server.Addr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Printf("Error starting server: %v", err)
+
+	case sig := <-shutdown:
+		log.Printf("Server is shutting down due to %v signal", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("Could not stop server gracefully: %v", err)
+			server.Close()
+		}
+	}
+
+}
