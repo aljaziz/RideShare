cd89eb5fb45e865840364128278997efd72e645d
aljaziz <turjatheharbingerofdeath@gmail.com>
2026-06-22 18:38:04 +0600

    web socket handlers added

---
+++ services/api-gateway/ws.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"aljaziz/RideShare/shared/contracts"
+	"aljaziz/RideShare/shared/util"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handleRidersWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("WebSocket upgrade failed: %v", err)
+		return
+	}
+
+	defer conn.Close()
+
+	userID := r.URL.Query().Get("userID")
+	if userID == "" {
+		log.Println("No user ID provided")
+		return
+	}
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading messageL %v", err)
+			break
+		}
+		log.Printf("Received message: %s", message)
+	}
+}
+
+func handleDriverWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("WebSocket upgrade failed: %c", err)
+		return
+	}
+
+	defer conn.Close()
+
+	userID := r.URL.Query().Get("userID")
+	if userID == "" {
+		log.Println("No user ID provided")
+		return
+	}
+
+	packageSlug := r.URL.Query().Get("packageSlug")
+	if packageSlug == "" {
+		log.Println("No package slug provided")
+		return
+	}
+
+	type Driver struct {
+		Id             string `json:"id"`
+		Name           string `json:"name"`
+		ProfilePicture string `json:"profilePicture"`
+		CarPlate       string `json:"carPlate"`
+		PackageSlug    string `json:"packageSlug"`
+	}
+
+	msg := contracts.WSMessage{
+		Type: "driver.cmd.register",
+		Data: Driver{
+			Id:             userID,
+			Name:           "Tiago",
+			ProfilePicture: util.GetRandomAvatar(1),
+			CarPlate:       "ABC123",
+			PackageSlug:    packageSlug,
+		},
+	}
+
+	if err := conn.WriteJSON(msg); err != nil {
+		log.Printf("Error sending message: %v", err)
+		return
+	}
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading message: %v", err)
+			break
+		}
+
+		log.Printf("Received message: %s", message)
+	}
+}
