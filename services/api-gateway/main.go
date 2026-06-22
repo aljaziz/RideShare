29a4ef80fc09a451dec44007a6c846cd4be9723e
aljaziz <turjatheharbingerofdeath@gmail.com>
2026-06-22 18:37:19 +0600

    websocket for drivers and riders added

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"aljaziz/RideShare/shared/env"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -16,10 +21,40 @@ func main() {
 	godotenv.Load()
 	log.Println("Starting API Gateway")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello from API Gateway"))
-	})
+	mux := http.NewServeMux()
 
-	http.ListenAndServe(httpAddr, nil)
+	mux.HandleFunc("POST /trip/preview", enableCORS(handleTripPreview))
+	mux.HandleFunc("/ws/drivers", handleDriverWebSocket)
+	mux.HandleFunc("/ws/riders", handleRidersWebSocket)
+
+	server := &http.Server{
+		Addr:    httpAddr,
+		Handler: mux,
+	}
+
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Printf("Server listenting on %s", httpAddr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Printf("Error stating the server: %v", err)
+
+	case sig := <-shutdown:
+		log.Printf("Server is shutting down due to %v signal", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("Could not stop the server gracefully: %v", err)
+			server.Close()
+		}
+	}
 }
