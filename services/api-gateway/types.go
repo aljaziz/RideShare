24d3701b6a55d24f50826572b82b21c9e8e0a09c
aljaziz <turjatheharbingerofdeath@gmail.com>
2026-06-29 19:31:30 +0600

    toProto function added for api gateway service

--- services/api-gateway/types.go
+++ services/api-gateway/types.go
@@ -1,9 +1,26 @@
 package main
 
-import "aljaziz/RideShare/shared/types"
+import (
+	pb "aljaziz/RideShare/shared/proto/trip"
+	"aljaziz/RideShare/shared/types"
+)
 
 type previewTripRequest struct {
 	UserID      string           `json:"userID"`
 	Pickup      types.Coordinate `json:"pickup"`
 	Destination types.Coordinate `json:"destination"`
+}
+
+func (p *previewTripRequest) toProto() *pb.PreviewTripRequest {
+	return &pb.PreviewTripRequest{
+		UserID: p.UserID,
+		StartLocation: &pb.Coordinate{
+			Latitude:  p.Pickup.Latitude,
+			Longitude: p.Pickup.Longitude,
+		},
+		EndLocation: &pb.Coordinate{
+			Latitude:  p.Destination.Latitude,
+			Longitude: p.Destination.Longitude,
+		},
+	}
 }
